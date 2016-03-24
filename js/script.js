af94ef5090be191f210342d5f9befc0dd44f088b
Fernanda Lowe <YOUR EMAIL ADDRESS>
2016-03-24 16:46:13 -0700

    add more content to the script file

--- js/script.js
+++ js/script.js
@@ -26,19 +26,32 @@ $(document).ready(function() {
     console.log(eyesInput);
     console.log(nationalityInput);
 
-    if (genderInput === "female" && ageInput === "under20" && eyesInput === "green" && nationalityInput === "Brazilian") {
-      alert ("Thank you for submitting your profile!");
-      $(".Mike, .Matt, .John").show();
-    } else if (genderInput === "female" && ageInput  ==="over50") {
-      $(".Obama, .John").show();
-      alert ("Thank you for submitting your profile!");
-    } else if (genderInput === "female" && ageInput  ==="over40") {
-      $('.John').show();
-      alert ("Thank you for submitting your profile!");
-    } else if (genderInput === "female" && ageInput  ==="under30") {
-      $('.Matt').show();
-      alert ("Thank you for submitting your profile!");
+    if (genderInput === "female") {
+      if (ageInput === "under20" && eyesInput === "green" && nationalityInput === "Brazilian") {
+        $(".Mike, .Matt, .John").show();
+      } else if (ageInput  ==="over50") {
+        $(".Obama, .John").show();
+      } else if ( ageInput  ==="over40") {
+        $('.John').show();
+      } else if (ageInput  ==="under30") {
+        $('.Matt').show();
+
+      }
+      else {
+        if (genderInput === "male" && ageInput === "under20" && eyesInput === "blue" && nationalityInput === "American") {
+          alert ("Thank you for submitting your profile!");
+          $(".Sara, .Barbara").show();
+        } else if (genderInput === "male" && ageInput  ==="over50") {
+          $(".Michelle, .Angela").show();
+        } else if (genderInput === "male" && ageInput  ==="over40") {
+          $('.Michelle, .Barbara').show();
+        } else if (genderInput === "male" && ageInput  ==="under30") {
+          $('.Sara .Angela').show();
+
+      }
     }
+
+
   });
 
 });
